1899e393575b84de3875aa9ad4774a3882290b78
Ridha <45142884+FethiBen@users.noreply.github.com>
2019-01-14 09:16:12 +0100

    Update DashboardFragment.kt

--- app/src/main/java/com/fethiben/itransfoapp/DashboardFragment.kt
+++ app/src/main/java/com/fethiben/itransfoapp/DashboardFragment.kt
@@ -163,7 +163,7 @@ class DashboardFragment : Fragment() {
                     msg.setTextColor(resources.getColor(R.color.colorWhite))
                     msg.text = data.getString("msg").toString()
                     msg.layoutParams = ViewGroup.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
-                    msg.setPadding(35, 35, 15, 15)
+                    msg.setPadding(35, 15, 15, 15)
                     //msg.setMargins(25, 0, 0, 0);
                     msg.textSize = 25.toFloat()
                     try {
